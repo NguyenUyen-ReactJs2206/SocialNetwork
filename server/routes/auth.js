c60317fdafaf4618bf4e8d1d32f3e8c772968d1b
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-09-16 16:20:55 +0700

    Authentication and User Routes

---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { login } from "../controllers/auth.js";
+
+const authRoutes = express.Router();
+
+authRoutes.post("/login", login);
+export default authRoutes;
