776730d109b1abec8600e597abe3ba27f1cc45cb
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-09-20 00:21:03 +0700

    Custom theme dark and light

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import Login from "scenes/loginPage/Login";
 import Home from "scenes/homePage/Home";
 import Profile from "scenes/profilePage/Profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/profile/:userId" element={<Profile />}></Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/profile/:userId" element={<Profile />}></Route>
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
