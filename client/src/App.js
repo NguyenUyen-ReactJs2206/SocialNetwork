70cc628c7ebb80b40c7e11f6304b429fa54aacb4
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-09-19 23:51:14 +0700

    Setup Folder and redux authSlice

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "scenes/loginPage/Login";
+import Home from "scenes/homePage/Home";
+import Profile from "scenes/profilePage/Profile";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/profile/:userId" element={<Profile />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
