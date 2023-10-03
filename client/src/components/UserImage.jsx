5ea1befe8b473dd68258f17ef5a6f5a45a7de007
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-03 23:57:29 +0700

    fix show image

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -3,15 +3,14 @@ import React from "react";
 
 export default function UserImage({ image, size = "60px" }) {
   console.log(image);
-  const imageLink = `http://localhost:3001/assets/${image}`;
   return (
     <Box width={size} height={size}>
       <img
         style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
-        src={imageLink}
         alt="user"
+        src={`http://localhost:3001/assets/${image}`}
       />
     </Box>
   );
