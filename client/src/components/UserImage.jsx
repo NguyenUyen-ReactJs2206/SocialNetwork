16df8186d9d317ce457060cdae0502ac1143bbfa
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-03 23:49:02 +0700

    Show SideHome

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -2,13 +2,15 @@ import { Box } from "@mui/material";
 import React from "react";
 
 export default function UserImage({ image, size = "60px" }) {
+  console.log(image);
+  const imageLink = `http://localhost:3001/assets/${image}`;
   return (
     <Box width={size} height={size}>
       <img
         style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
-        src={`http://localhost:3001/assets/${image}`}
+        src={imageLink}
         alt="user"
       />
     </Box>
