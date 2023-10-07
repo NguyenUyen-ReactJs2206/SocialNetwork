6bd37c509ecf3c3b119f36497e1dd7d77fc2dd08
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-07 23:43:21 +0700

    Add Friend

--- client/src/scenes/homePage/Home.jsx
+++ client/src/scenes/homePage/Home.jsx
@@ -2,6 +2,8 @@ import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar/Navbar";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
@@ -30,7 +32,13 @@ export default function Home() {
           <PostsWidget userId={_id} />
         </Box>
 
-        {isNonMobieleScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobieleScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
       </Box>
     </Box>
   );
