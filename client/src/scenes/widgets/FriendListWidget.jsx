7d5c864b9cf9aa1929dbb196689fc0c6bc5b2cb0
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-07 23:51:11 +0700

    Fix array.map is not a function

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -39,15 +39,16 @@ export default function FriendListWidget({ userId }) {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
-          <Friend
-            key={friend._id}
-            friendId={friend._id}
-            name={`${friend.firstName} ${friend.lastName}`}
-            subtitle={friend.occupation}
-            userPicturePath={friend.picturePath}
-          />
-        ))}
+        {Array.isArray(friends) &&
+          friends.map((friend) => (
+            <Friend
+              key={friend._id}
+              friendId={friend._id}
+              name={`${friend.firstName} ${friend.lastName}`}
+              subtitle={friend.occupation}
+              userPicturePath={friend.picturePath}
+            />
+          ))}
       </Box>
     </WidgetWrapper>
   );
